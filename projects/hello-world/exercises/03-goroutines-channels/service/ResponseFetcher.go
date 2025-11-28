d2872bb2638005360ac5f0325c3fcdfb9f78bc6f
viraj.pansuriya <viraj.pansuriya@verse.in>
2025-11-28 15:42:31 +0530

    Task 3 Improvement

--- projects/hello-world/exercises/03-goroutines-channels/service/ResponseFetcher.go
+++ projects/hello-world/exercises/03-goroutines-channels/service/ResponseFetcher.go
@@ -12,26 +12,28 @@ var (
 )
 
 type ResponseFetcher interface {
-	Fetch(urls []string) []model.Result
+	Fetch(urls []string)
 }
 
 type UrlResponseFetcher struct {
 }
 
-func (ur *UrlResponseFetcher) Fetch(urls []string) []model.Result {
-	results := make([]model.Result, len(urls))
+func (ur *UrlResponseFetcher) Fetch(urls []string) {
+	results := make(map[string]model.Result, len(urls))
+
 	ch := make(chan model.Result)
 	startTime := time.Now()
 	for index := 0; index < len(urls); index++ {
 		go model.FetchURL(urls[index], ch)
 	}
 	for index := 0; index < len(urls); index++ {
-		results[index] = <-ch
-		fmt.Println("Fetched result from URL", urls[index], " is : ", results[index])
+		res := <-ch
+		results[res.URL] = res
+		fmt.Println("Fetched result from URL", res.URL, " is : ", res)
 	}
 	endTime := time.Since(startTime)
 	fmt.Println("Total Time taken for batch request is ", endTime.Milliseconds())
-	return results
+	return
 }
 
 func GetResponseFetcherInstance() ResponseFetcher {
